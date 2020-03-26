34c3d5a1419079261f3c56e4c7da024f9e0c280f
Hamed Darragi <hd@xanweb.com>
2020-03-26 15:19:58 +0100

    Remove unnecessary manifest file

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,7 @@ const mix = require('laravel-mix');
 
 mix.sass('css/scss/dashboard/ios-toggle-button.scss', 'css/dashboard')
     .disableNotifications();
+
+// Disable mix-manifest.json
+// @see https://github.com/JeffreyWay/laravel-mix/issues/580
+Mix.manifest.refresh = _ => void 0;
