27f0e43dfc72955097040437fe66ea8a8216f3ca
Hamed Darragi <hd@xanweb.com>
2021-05-27 12:16:02 +0100

    Extend Concrete Form: label now can support tooltip and required html

---
+++ src/Service/Form.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Xanweb\HtmlHelper\Service\Form;
+
+use Concrete\Core\Form\Service\Form as CoreForm;
+use HtmlObject\Element;
+
+class Form extends CoreForm
+{
+    /**
+     * Creates a label tag.
+     *
+     * @param string $forFieldID the id of the associated element
+     * @param string $innerHTML the inner html of the label
+     * @param array $miscFields additional fields appended to the element (a hash array of attributes name => value), possibly including 'class'
+     *                          added support for tooltip in $miscFields: $miscFields['tooltip'] = t('My Help Text')
+     *
+     * @return string
+     */
+    public function label($forFieldID, $innerHTML, $miscFields = [])
+    {
+        if (isset($miscFields['tooltip'])) {
+            $innerHTML .= ' ' . Element::create(
+                'i',
+                '',
+                [
+                    'title' => $miscFields['tooltip'],
+                    'class' => 'launch-tooltip fas fa-question-circle',
+                ]
+            );
+
+            unset($miscFields['tooltip']);
+        }
+
+        $required = isset($miscFields['required']) && $miscFields['required'];
+
+        unset($miscFields['required']);
+
+        $labelHtml = parent::label($forFieldID, $innerHTML, $miscFields);
+
+        if ($required) {
+            $labelHtml .= ' <div class="float-right"><span class="text-muted small">' . t('Required') . '</span></div>';
+        }
+
+        return $labelHtml;
+    }
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,11 +3,14 @@
 namespace Xanweb\HtmlHelper;
 
 use Xanweb\Common\Service\Provider as FoundationServiceProvider;
+use Xanweb\HtmlHelper\Service\Form\Form;
 
 class ServiceProvider extends FoundationServiceProvider
 {
     public function _register(): void
     {
+        $this->app->bind(\Concrete\Core\Form\Service\Form::class, Form::class);
+        $this->app->singleton('helper/form', Form::class);
         $this->app->singleton('helper/form/ios_toggler', function ($app) {
             return new Service\Form\IosTogglerWidget($app['helper/form']);
         });
